c403ce14f59cc6c73d1f7590ac18ad06b5b21e03
hsaeed <hs35>
2024-12-01 11:15:04 +0300

    almost done

--- client.py
+++ client.py
@@ -6,27 +6,30 @@ PORT = 12345        # Port used by the server
 
 # Create a client socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+clientname=input('Enter your name: ')
 try:
     # Connect to the server
     client_socket.connect((HOST, PORT))
+    client_socket.sendall(clientname.encode())
     print("Connected to the server.")
     while True:
         print("1 - search by headlines")
         print("2 - List of sources")
         print("3 - Quit")
         x=int(input("choise your option "))
-        if x==1:
-            print('1 - search for kewyords ')
-            print('2 - search by catagory ')
-            print('3 - search for country ')
-            print('4 - list all new headlines ')
-            print('5 - Back to main menue')
-            while True:
+        while True:
+            if x==1:
+                print('1 - search for kewyords ')
+                print('2 - search by catagory ')
+                print('3 - search for country ')
+                print('4 - list all new headlines ')
+                print('5 - Back to main menue')
+
                 y=int(input("choise your option "))
                 if y==1:
                     keyword = input("Enter your keyword: ")
                     client_socket.sendall((keyword+'h1').encode())
+
                 elif y==2:
                     category = input("Enter your category: ")
                     client_socket.sendall((category+'h2').encode())
@@ -38,32 +41,36 @@ try:
                 elif y==5:
                     break
                 else:
-                    print("Invalid option. Please choose a valid option.")
-                    
-        if x==2:
-            while True:
-                print('1 - list all sources by category ')
-                print('2 - list all sources by country ')
-                print('3 - list all sources by languege ')
-                print('4 - list all sources ')
-                print('5 - Back to main menu')
-                y=int(input('choise your option '))
-                if y==1:
-                    category = input("Enter your category: ")
-                    client_socket.sendall((category+'s1').encode())
-                elif y==2:
-                    country = input("Enter your country: ")
-                    client_socket.sendall((country+'s2').encode())
-                elif y==3:
-                    language = input("Enter your language: ")
-                    client_socket.sendall((language+'s3').encode())
-                elif y==4:
-                    client_socket.sendall('s4'.encode())
-                elif y==5:
-                    break
-                else:
-                    print("Invalid option. Please choose a valid option.")
-                                          
+                        print("Invalid option. Please choose a valid option.")
+                recived=client_socket.recv(4000)
+                results=json.loads(recived.decode('utf-8'))
+                for i in results:
+                    print(i)
+        while True:                
+            if x==2:
+
+                    print('1 - list all sources by category ')
+                    print('2 - list all sources by country ')
+                    print('3 - list all sources by languege ')
+                    print('4 - list all sources ')
+                    print('5 - Back to main menu')
+                    y=int(input('choise your option '))
+                    if y==1:
+                        category = input("Enter your category: ")
+                        client_socket.sendall((category+'s1').encode())
+                    elif y==2:
+                        country = input("Enter your country: ")
+                        client_socket.sendall((country+'s2').encode())
+                    elif y==3:
+                        language = input("Enter your language: ")
+                        client_socket.sendall((language+'s3').encode())
+                    elif y==4:
+                        client_socket.sendall('s4'.encode())
+                    elif y==5:
+                        break
+                    else:
+                        print("Invalid option. Please choose a valid option.")
+
 except ConnectionRefusedError:
     print("Connection to the server failed.")
 finally:
--- server.py
+++ server.py
@@ -3,38 +3,152 @@ import requests
 import json
 import socket
 import threading
+import re
+from datetime import datetime
 
 #to seperate the users data to make every user has his own
 thread_data=threading.local()
-haedline = "https://newsapi.org/v2/top-headlines?"
-sources='https://newsapi.org/v2/top-headlines/sources?'
+haedline = "https://newsapi.org/v2/top-headlines?apiKey=4411d3bdab63427c91fcef22bed1b3f0"
+sources='https://newsapi.org/v2/top-headlines/sources?apiKey=4411d3bdab63427c91fcef22bed1b3f0'
+
+def requested_data(data,type,torequest,recdata):
+
+    if torequest=='':
+        torequest=type
+    else:
+        torequest+=' , '+type
+    if recdata=='':
+        recdata=data
+    else:
+        recdata+=' , '+data
+
+    return [recdata,torequest]
+    
+def response(selected_option, user_input,filename):
+    try:
+        # Retrieve data once when the server starts
+        with open(filename, 'r') as ofile:
+            info = json.load(ofile)
+
+        selected_info = "."
+        for news in info:
+            # Process flight data based on the selected option
+            if selected_option == '1':
+                if news['flight_status'] == 'landed':
+                    selected_info += f"***Here are your required information about arrived flights ✅***\n"
+                    selected_info += f"\n"  
+                    selected_info += f"Flight IATA code: {news['flight']['iata']}\n"
+                    selected_info += f"Departure Airport: {news['departure']['airport']}\n"
+                    selected_info += f"Arrival Time: {news['arrival']['estimated']}\n"
+                    selected_info += f"Arrival Terminal: {news['arrival']['terminal']}\n"
+                    selected_info += f"Arrival Gate: {news['arrival']['gate']}\n"
+                    selected_info += "\n"
+                    selected_info += "=====================================\n"
+            elif selected_option == '2':
+                if news['arrival']['delay'] is not None:
+                    selected_info += f"***Here are your required information about delayed flights ⏱️***\n"
+                    selected_info += f"\n"
+                    selected_info += f"Flight IATA code: {news['flight']['iata']}\n"
+                    selected_info += f"Departure Airport: {news['departure']['airport']}\n"
+                    selected_info += f"Original departure Time: {news['departure']['actual']}\n"
+                    selected_info += f"Estimated Arrival Time: {news['arrival']['estimated']}\n"
+                    selected_info += f"Arrival terminal: {news['departure']['terminal']}\n"
+                    selected_info += f"delay : {news['arrival']['delay']}\n"
+                    selected_info += f"Arrival Gate: {news['arrival']['gate']}\n"
+                    selected_info += "\n"
+                    selected_info += "=====================================\n"
+            elif selected_option == '3':
+                if news['departure']['iata'] == user_input:
+                    selected_info += f"***Required information about specific airport/city 👇***\n"
+                    selected_info += f"\n"
+                    selected_info += f"Flight IATA code: {news['flight']['iata']}\n"
+                    selected_info += f"Departure Airport: {news['departure']['airport']}\n"
+                    selected_info += f"Original departure Time: {news['departure']['actual']}\n"
+                    selected_info += f"Estimated Arrival Time: {news['arrival']['estimated']}\n"
+                    selected_info += f"Arrival Gate: {news['arrival']['gate']}\n"
+                    selected_info += f"Departure Gate: {news['departure']['gate']}\n"
+                    selected_info += f"flight_status: {news['flight_status']}\n"
+                    selected_info += "\n"
+                    selected_info += "=====================================\n"
+            elif selected_option == '4':
+                if news['flight']['iata'] == user_input:
+                    selected_info += f"***Here are your required information about this flight 📅***\n"
+                    selected_info += f"\n"
+                    selected_info += f"Flight IATA code: {news['flight']['iata']}\n"
+                    selected_info += f"Departure Airport: {news['departure']['airport']}\n"
+                    selected_info += f"Departure Gate: {news['departure']['gate']}\n"
+                    selected_info += f"Departure terminal: {news['departure']['terminal']}\n"
+                    selected_info += f"Arrival airport: {news['arrival']['airport']}\n"
+                    selected_info += f"Arrival Gate: {news['arrival']['gate']}\n"
+                    selected_info += f"Arrival terminal: {news['arrival']['terminal']}\n"
+                    selected_info += f"flight_status: {news['flight_status']}\n"
+                    selected_info += f"scheduled Departure time: {news['departure']['scheduled']}\n"
+                    selected_info += f"scheduled arrival time: {news['arrival']['scheduled']}\n"
+                    selected_info += "=====================================\n"
+    except FileNotFoundError:
+        selected_info = "Error: File not found."
+    except json.JSONDecodeError:
+        selected_info = "Error: Unable to decode JSON file."
+    except Exception as e:
+        selected_info = f"An unexpected error occurred: {str(e)}"
+        
+    return selected_info
+
 
 def handle_client(conn,adrr):
     '''a function to handle the client request'''
-    print("connected to client at", addr)
     thread_data.client_data={} #dict for every client
+    url=''
+    torequest=''
+    recdata=''
+    tp=''
     try:
         while True:
             data = conn.recv(1024)
-            print(data[-2])
             # to determine wither its headline request or sources
             if data[-2]==115: # the number is for s in ascii table
+                if url==haedline:
+                    thread_data.client_data={}
+                    torequest=''
+                    recdata=''
+
                 url=sources
+
             elif data[-2]==104: # the number is for h in ascii table
-                url=haedline
+                if url==sources:
+                    thread_data.client_data={}  
+                    torequest=''
+                    recdata=''
+
+                url=haedline 
+            
+
             if not data:
                 break
             '''distinguish between the choise of client'''
             if data[-2:]==b'h1':
                 thread_data.client_data['q']=data[:-2].decode()
+                tp='keywords'
+                
             elif data[-2:]==b'h2' or data[-2:]==b's1':
                 thread_data.client_data['category']=data[:-2].decode()
+                tp='category'
+                
             elif data[-2:]==b'h3'or data[-2:]==b's2':
                 thread_data.client_data['country']=data[:-2].decode()
+                tp='country'
             elif data[-2:]==b's3':
-                thread_data.client_data['languege']=data[:-2].decode()
+                thread_data.client_data['language']=data[:-2].decode()
+                tp='language'
+            elif data[-2:]==b'h4':
+                 thread_data.client_data['country']='us'
+                    
             
-            prams={'apiKey':'69252012c3fb4382afcf446fa407a866'}
+            cdata=requested_data(data[:-2].decode(),tp,torequest,recdata)
+            recdata=cdata[0]
+            torequest=cdata[1]
+            
+            prams={}
             #move the client info to prameter dict (if chosen)
             if 'q' in thread_data.client_data:
                 prams['q']=thread_data.client_data['q']
@@ -42,20 +156,59 @@ def handle_client(conn,adrr):
                 prams['category']=thread_data.client_data['category']
             if 'country' in thread_data.client_data:
                 prams['country']=thread_data.client_data['country']
-            if 'languege' in thread_data.client_data:
-                prams['language']=thread_data.client_data['languege']
+            if 'language' in thread_data.client_data:
+                prams['language']=thread_data.client_data['language']
+            
+            print(client_name,'request by',torequest,'with',recdata)
             
             response = requests.get(url=url,params=prams)
             print(response.url)
-            
             if response.status_code == 200:
                 data = response.json()
-                 # Save data to a file
-                with open("posts.json", "w") as file:
+                with open('aa.json', "w") as file:
                      json.dump(data, file, indent=4)
                      print("[SUCCESS] Data saved to 'posts.json'")
+                article_details = []
+                if url==haedline:
+                # iterate over the articles and print their details
+                   
+
+                    for article in data.get("articles", []):
+                        
+                            source_name = article.get("source", {}).get("name", "No source name available")
+                            title = article.get("title", "No title available")
+                            author = article.get("author", "No author available")
+                            publication_date = article.get("publishedAt", "No publication date available")
+                            description = article.get("description", "No description available")
+                            url = article.get("url", "No url available")
+                            tobject =datetime.strptime(publication_date, "%Y-%m-%dT%H:%M:%SZ")
+                            formatted_date =tobject.strftime("%b %d %Y, %I:%M %p")
+                            # Convert the date and time to a more readable format
+                            print(formatted_date)
+                            article_details.append({"source_name": source_name, "title": title, "author": author,'url': url, "description": description, "published At":formatted_date })
+                            breiflist=[{"title": article_details["title"], "author": a["author"], "published-day": a["published-day"], "published-time": a["published-time"]}for a in article]
+                            results=json.dumps(breiflist)
+                            conn.sendall(results.encode())
+                    else:
+                        print("[ERROR] No article data found")
+                        conn.sendall(b'No articles found')
+                elif url==sources:
+                     for article in data.get("articles", []):
+                        source_name = article.get("source", {}).get("name", "No source name available")
+                        title = article.get("title", "No title available")
+                        author = article.get("author", "No author available")
+
+                        article_details.append({"source_name": source_name, "title": title, "author": author })
+
+               
+                nameoffile=client_name+' '+torequest+' '+recdata+' '+'A19'
+                nameoffile=re.sub("\s+", "_",nameoffile)
+                
+               
             else:
-                print(f"[ERROR] Status Code: {response.status_code}")
+                print("[ERROR] Status Code:",response.status_code)
+            
+
 
             
     except Exception as e:
@@ -74,5 +227,7 @@ print("the server is listening on localhost port 12345")
 #accept connection from clients ans start thread
 while True:
     conn,addr=server_socket.accept()
+    client_name=conn.recv(100).decode()
+    print(client_name,'has connected with address',addr)
     thread=threading.Thread(target=handle_client,args=(conn,addr))
     thread.start()
